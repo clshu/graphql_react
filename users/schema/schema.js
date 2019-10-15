4f91f0af77d701d3892bb019b4455a38c9b9f968
clshu <ccss0630@yahoo.com>
2019-10-14 17:49:17 -0700

    Create a new schema in JS code with hard-coded data and get QrpahiQL works

---
+++ users/schema/schema.js
@@ -0,0 +1,41 @@
+const graphql = require('graphql')
+const _ = require('lodash')
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = graphql
+
+const users = [
+    { id: '23', firstName: 'Bill', age: 20 },
+    { id: '40', firstName: 'Alan', age: 30 }
+]
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    }
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString }},
+            resolve(parent, args) {
+                return _.find(users, { id: args.id })
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
+
