b56e34913cc495c261e72e8d94df8a7c05d4a518
clshu <ccss0630@yahoo.com>
2019-10-15 19:31:51 -0700

    Make title clickable and go to SongDetail and add back Link in SongDetail to go back /

--- Lyrical-GraphQL/client/components/SongDetail.js
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import fetchSong from "../queries/fetchSong";
+import { Link } from "react-router";
 
 class SongDetail extends Component {
   render() {
     const { song } = this.props.data;
     if (!song) return <div></div>;
 
-    return <div>{song.title}</div>;
+    return (
+      <div>
+        <Link to="/">Back</Link>
+        <div>{song.title}</div>
+      </div>
+    );
   }
 }
 
