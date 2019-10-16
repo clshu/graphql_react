925e31a1c25cacfb5b6bea93eeabc2a13bbd01cd
clshu <ccss0630@yahoo.com>
2019-10-15 23:01:38 -0700

    Add LyricList to SongDetail

---
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class LyricList extends Component {
+  renderList() {
+    return this.props.lyrics.map(({ id, content }) => {
+      return (
+        <li key={id} className="collection-item">
+          {content}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return <ul className="collection">{this.renderList()}</ul>;
+  }
+}
+
+export default LyricList;
--- Lyrical-GraphQL/client/components/SongDetail.js
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -3,16 +3,17 @@ import { graphql } from "react-apollo";
 import fetchSong from "../queries/fetchSong";
 import { Link } from "react-router";
 import LyricCreate from "./LyricCreate";
+import LyricList from "./LyricList";
 
 class SongDetail extends Component {
   render() {
     const { song } = this.props.data;
     if (!song) return <div></div>;
-
     return (
       <div>
         <Link to="/">Back</Link>
-        <div>{song.title}</div>
+        <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
         <LyricCreate songId={song.id} />
       </div>
     );
