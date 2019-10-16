910738a3b3cd5a175cdc2f91820f9e13c28e94d9
clshu <ccss0630@yahoo.com>
2019-10-15 19:24:55 -0700

    Add SongDetail with path /songs/:id

---
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import fetchSong from "../queries/fetchSong";
+
+class SongDetail extends Component {
+  render() {
+    const { song } = this.props.data;
+    if (!song) return <div></div>;
+
+    return <div>{song.title}</div>;
+  }
+}
+
+export default graphql(fetchSong, {
+  // This props belongs to graphql layer
+  // It can see props passed down from React router
+  // /:id is props.params.id
+  options: props => {
+    return { variables: { id: props.params.id } };
+  }
+})(SongDetail);
